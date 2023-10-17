354cc1909a033a2bcd730b10c78acacde8c6f73f
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2023-10-17 22:20:54 +0900

    [update] delete shutdown from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include <rclcpp/rclcpp.hpp>
 #include <memory>
+#include <rclcpp/rclcpp.hpp>
 #include <ros2_whill/ros2_whill_component.hpp>
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     rclcpp::init(argc, argv);
     auto whill_node = std::make_shared<WhillController>();
     rclcpp::spin(whill_node);
-    whill_node->shutdown_whill();
     rclcpp::shutdown();
     return 0;
 }
