3a197988d2b560262ef1432e057bbc38aa36cb29
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2023-11-29 21:39:09 +0900

    [update] add robot_state_publisher - どうして...

--- launch/modelc.launch.py
+++ launch/modelc.launch.py
@@ -10,19 +10,24 @@ from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
     pkg_dir = get_package_share_directory("ros2_whill")
     list = [
         Node(
-            package='ros2_whill',
-            executable='ros2_whill',
-            namespace='',
-            output='screen',
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            namespace="",
+            remappings=[("/joint_states", "/whill/states/jointState")],
+        ),
+        Node(
+            package="ros2_whill",
+            executable="ros2_whill",
+            namespace="",
+            output="screen",
             respawn=True,
-            parameters=[
-                os.path.join(pkg_dir, "params", "sample_param.yaml")
-            ]
+            parameters=[os.path.join(pkg_dir, "params", "sample_param.yaml")],
         ),
     ]
-    
+
     return LaunchDescription(list)
